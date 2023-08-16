40df5bddeae05b23345f6a7ab27676190df1dd30
Barbara Joy <barbarajoydneves@gmail.com>
2023-08-16 14:40:20 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "restaurants#index"
+  resources :restaurants do
+    collection do
+      get :index, :show
+    end
+    resources :reviews, only: %i[new create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+dozo = { name: 'Dozo', address: 'Gambetta', phone_number: "0749204711", category: "chinese" }
+tripletta = { name: 'Tripletta', address: 'Chartrons', phone_number: "0776204711", category: "chinese" }
+caramel = { name: 'Caramel', address: 'Saint Catherine', phone_number: "0749764711", category: "french" }
+zepherine = { name: 'Zepherine', address: 'Chartrons', phone_number: "0749321111", category: "chinese" }
+osteria = { name: 'Osteria', address: 'Gambetta', phone_number: "0749984411", category: "italian" }
+
+
+[dozo, tripletta, caramel, zepherine, osteria].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+review1 = { content: 'Great', rating: 5 }
+review2 = { content: 'Bad', rating: 1 }
+
+[review1, review2].each do |attributes|
+  review = Review.create!(attributes)
+  puts 'Created review'
+end
